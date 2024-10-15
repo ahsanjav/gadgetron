d03c8f71fc6cbf15d56cb4cb379f1de19cea5bee
ahsan javed <ahsan.javed@zohomail.com>
2024-10-15 17:13:58 +0000

    added lit functions

--- toolboxes/core/gpu/cuNDArray_elemwise.h
+++ toolboxes/core/gpu/cuNDArray_elemwise.h
@@ -27,6 +27,21 @@
 
 #include "cuNDArray.h"
 
+namespace nhlbi_toolbox
+{
+    namespace cuda_utils
+    {
+        template <typename T>
+        Gadgetron::cuNDArray<T> cuexp(Gadgetron::cuNDArray<T> &x);
+
+        template <typename T>
+        Gadgetron::cuNDArray<T> cucos(Gadgetron::cuNDArray<T> &x);
+
+        template <typename T>
+        Gadgetron::cuNDArray<T> cusin(Gadgetron::cuNDArray<T> &x);
+
+    } // namespace cuda_util
+} // namespace nhlbi_toolbox
 
 namespace Gadgetron{
 
@@ -252,4 +267,19 @@ namespace Gadgetron{
      * @param[in] p p value of the shrinkage. Should be less than 1 and more than 0.
      */
     template<class T> void pshrinkd ( cuNDArray<T> *x, cuNDArray<typename realType<T>::Type> *s, typename realType<T>::Type gamma,typename realType<T>::Type p, cuNDArray<T> *out = 0x0 );
+
+    /**
+//    * @brief Construct a complex array from a real array and imag array.
+//    * @param[in] x real array, y imag array.
+//    * @return A new complex array containing the input array in the real component and imag elements in the imaginary component.
+//    */
+  template<class T> boost::shared_ptr< cuNDArray<T> > cureal_imag_to_complex( const cuNDArray<typename realType<T>::Type> *x, const cuNDArray<typename realType<T>::Type> *y );
+
+//      /**
+//  * @brief Construct a complex array from a imag array.
+//  * @param[in] x Input array.
+//  * @return A new complex array containing the input array in the imag component and zeros in the imaginary component.
+//  */
+
+template <class T> boost::shared_ptr<cuNDArray<T>> imag_to_complex(const cuNDArray<typename realType<T>::Type>* x);
 }
